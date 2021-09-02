35a6b85c1d9abc5cf27857e12d2a558ec4169cce
Filipe Calegario <fcac@cin.ufpe.br>
2021-09-01 21:16:55 -0300

    Updating kit list

--- js/project-list.js
+++ js/project-list.js
@@ -29,7 +29,7 @@ function initializeProjectList() {
                 console.log(selected_project);
             })
             .selectAll("option")
-            .data(listProjects);
+            .data(listProjects, d => d.id);
         options.enter()
             .append("option")
             .attr("value", (d) => { return d.id })
@@ -41,10 +41,11 @@ function updateMapList(selected_project) {
     getProjectById(access_token, selected_project).then(project => {
         console.log(project.missions);
         let options = d3.select("#missions-list")
-            .on("change", () => {
+            .on("focus", () => {
                 // Print the selected mission id
                 let selected_mission = d3.select("#missions-list").property('value');
                 localStorage.setItem("selected_mission", selected_mission);
+                updateKitList(selected_mission);
                 console.log(selected_mission);
             })
             .selectAll("option")
@@ -56,6 +57,29 @@ function updateMapList(selected_project) {
         options.append("option")
             .attr("value", (d) => { return d.id })
             .text((d) => { return d.title });
+        options.exit().remove();
+    });
+}
+
+function updateKitList(selected_mission) {
+    getAllContentsByMissionId(access_token, selected_mission).then(mission => {
+        console.log(mission);
+        let options = d3.select("#kits-list")
+            .on("input", () => {
+                // Print the selected kit id
+                let selected_kit = d3.select("#kits-list").property('value');
+                localStorage.setItem("selected_kit", selected_kit);
+                console.log(selected_kit);
+            })
+            .selectAll("option")
+            .data(mission.content, d => d.kit.id);
+        options.enter()
+            .append("option")
+            .attr("value", (d) => { return d.id })
+            .text((d) => { return d.kit.title });
+        options.append("option")
+            .attr("value", (d) => { return d.id })
+            .text((d) => { return d.kit.title });
         options.exit().remove();
     });
 }
